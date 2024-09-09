77077108930f7bcdbc6710c0b822e38dc1262535
HenryBai <fovere.dbcd8@gmail.com>
2024-09-09 13:48:23 +0800

    2024/09/09

--- 476. Number Complement.cpp
+++ 476. Number Complement.cpp
@@ -24,3 +24,19 @@ public:
         return ans;
     }
 };
+
+// top solution:
+class Solution {
+public:
+    int findComplement(int num) {
+        if (num == 0) return 1;
+        
+        unsigned int mask = ~0;
+        
+        while (num & mask) {
+            mask <<= 1;
+        }
+        
+        return ~mask & ~num;
+    }
+};
